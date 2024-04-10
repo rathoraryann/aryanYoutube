f419562b8f197fa4b7f1b55abc5a7efe525960e3
ARYAN <arrathor1334@gmail.com>
2024-04-10 21:32:22 +0530

    apiFetchProblem

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/watch/:id' element={<Watch/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/store/reducers/getHomePageVideos.ts
@@ -0,0 +1,17 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { YOUTUBE_API_URL } from "../../utils/constants";
+import axios from "axios";
+import { RootState } from "..";
+
+const API_KEY = process.env.REACT_APP_YOTUBE_DATA_API_KEY;
+
+export const getHomePageVideos = createAsyncThunk(
+    "youtubeApp/home/PageVideos",
+    async(isNext:boolean, {getState}) =>{
+        const {
+            youtubeApp : {nextPageToken: nextPageTokenFromState, videos},
+        } = getState() as RootState;
+        const {data:{items,nextPageToken}} = await axios.get(`${YOUTUBE_API_URL}/search?maxResults=20&q="reactjs projects&key=${API_KEY}&part=snippet&type=video`);
+        console.log(items)
+    }
+)
